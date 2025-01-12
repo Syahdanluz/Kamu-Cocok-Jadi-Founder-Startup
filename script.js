81dbbd55bda44d1fc058562305ed5c7e244a5242
Syahdan Luza <bhatarafana@gmail.com>
2025-01-12 15:17:29 +0700

    add more startup ideas

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ const ideas = [
   "Jasa buat ngingetin temen yang suka lupa bayar patungan.",
   "Kursi bioskop yang otomatis mundur kalau ada orang lewat di depan kamu.",
   "App untuk bikin foto papan tulis hasil kelas kuliah kamu dimasukin ke AI untuk dibikin jadi catetan yang proper",
+  "Aplikasi buat nemuin temen yang sama-sama ketinggalan kereta dan terjebak di stasiun bareng.",
+  "Platform buat ngitungin seberapa banyak waktu kamu buang nonton video TikTok yang gak jelas dan bikin komparasi kalau waktu itu kamu pake buat kerjain hobi dan cita-cita kamu, beneifitnya seberapa",
+  "Platform rental temen buat dateng ke acara atau komunitas baru, biar ga malu-malu",
+  "App yang bakal consume semua data message, media, etc kamu untuk mereka bikin 'daftar hal yang harus kamu syukuri selama ini'. Count yo blessing~",
+  "Sistem aplikasi yang ngasih kamu ide barang lain yang bisa DIY atau gratis saat kamu cek barang di marketplace, biar kamu bisa hemat",
+  "App untuk bikinin trek jalan pagi/sore terbaik di rumah kamu, yang jalanannya manusiawi dan aman",
+  "Reminder di smart watch kamu untuk mindful breathing kalau stress level dan/atau blood pressure kamu di atas threshold",
+  "Renaissance pocket edition, app buat kamu riset ide-ide dalam disiplin ilmu apapun, biar kamu jadi polymath a la tokoh renaisans",
+  "Platform untuk bikin mahasiswa bisa lihat dosen mana yang paling banyak bikin mahasiswanya telat makan",
+  "WIFI management system untuk kafe biar bisa mutusin akses ke wifi buat customernya kalau dalam 4 jam dia cuma beli menu under 25K IDR",
+  "Update Google Maps biar user bisa saling chat sama kendaraan lain dalam range 50m",
+  
+
+
 
 ];
 
